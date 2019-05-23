af8b4cb29fef22048b20eb753025f62dcf2b2714
Michael Darr <medarr@email.wm.edu>
2019-05-23 18:26:23 -0400

    basic page layout/under construciton message

---
+++ src/components/Body.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container
+        fluid
+        grid-list-lg
+    >
+        <v-layout row wrap>
+            <v-flex xs12>
+                <v-card>
+                    <v-img
+                        src="img/construction.jpg"
+                        aspect-ratio="2.75"
+                    ></v-img>
+                    <v-card-title primary-title>
+                    <div>
+                        <div class="headline">
+                            Portfolio site under construction
+                        </div>
+                        <span>Please come back soon!</span>
+                    </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import NavbarComponent from './components/Navbar.vue';
+import BodyComponent from './components/Body.vue';
 import FooterComponent from './components/Footer.vue';
 import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+library.add(fas);
+
+Vue.use(Vuetify, {
+    iconfont: 'faSvg',
+});
 
 new Vue({
     el: '#app',
     components: {
         NavbarComponent,
+        BodyComponent,
         FooterComponent,
     },
     template: `
     <v-app>
-        <v-container grid-list-md text-xs-center>
+        <v-container>
             <navbar-component />
+            <body-component />
         </v-container>
         <footer-component />
     </v-app>
